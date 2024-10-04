cd8d8ebf674b4823cd5d4496a7f7b228b5a07308
Ali Qaredaqi <aligr.work@gmail.com>
2024-10-04 19:37:03 +0330

    person creator

---
+++ person creator/ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public Person(string firstName, string lastName, int age)
+	{
+		FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+	}
+
+}
